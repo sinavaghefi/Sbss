bde2715daf71e2528b42c2290012b335b1983de6
sinavaghefi <sinavaghefi22@gmail.com>
2016-06-09 06:31:41 -0700

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "webshot",
     "welcome"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {122774063},{191933191}--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V3
